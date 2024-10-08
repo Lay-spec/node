e5bc64626cf27bb32a316bd530912c45c3c44083
LAYLA COSTA MAURICIO <layla.cmauricio@senacsp.edu.br>
2024-10-08 17:18:14 -0300

    index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 //importando o express
 import express, {Request, Response} from 'express'
 import mainRoutes from './routes/index'
-
+import path from 'path'
 
 
 //usando express
 const server = express()
+
+//configuracao da pasta public
+server.use(express.static(path.join(__dirname,  'public')))
 
 //inserindo a rota no servidor
 server.use(mainRoutes)
